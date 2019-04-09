9b5ce0a0ae1a7b9cb814cf93ff4955649cc82f28
John Michael Singayan <33562760+jmsingayan25@users.noreply.github.com>
2019-04-09 21:43:35 +0800

    project

---
+++ public/js_config/firebase_config.js
@@ -0,0 +1,13 @@
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCwlSFA_4OzfZn4I7EDJ8nIXw17fc1DbQk",
+        authDomain: "jmstarcrete.firebaseapp.com",
+        databaseURL: "https://jmstarcrete.firebaseio.com",
+        projectId: "jmstarcrete",
+        storageBucket: "jmstarcrete.appspot.com",
+        messagingSenderId: "452890115480"
+      };
+
+    firebase.initializeApp(config);
+
